ab85fe37c1cd27cc94a36cbb6e983326e5f761aa
sushilkattel <info@sushilkattel.com>
2021-03-13 15:59:34 -0600

    Added volley and other required components for data retrieval

--- app/src/main/java/com/cloutstory/foshoweather/MainActivity.kt
+++ app/src/main/java/com/cloutstory/foshoweather/MainActivity.kt
@@ -3,19 +3,34 @@ package com.cloutstory.foshoweather
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import java.io.File
+import java.net.URL
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+
+    val city = "dhangadhi,np"
+    val apiKey = "de5ec822deb46bf3982d1809ff13d90e"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getWeatherIcon()
     }
-    fun getWeatherIcon () {
+
+    fun getWeatherJson() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey"
+
+    }
+    fun getWeatherIcon() {
         val weatherIconUrl = findViewById<ImageView>(R.id.weatherIcon)
         val weatherSunny = R.drawable.icon_sunny
         var weather: String
